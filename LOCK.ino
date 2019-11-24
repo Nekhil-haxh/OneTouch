d4e64f483523332a0613fa38f66e7808dec7af9d
Nekhil-haxh <53958279+Nekhil-haxh@users.noreply.github.com>
2019-11-24 08:04:36 +0530

    Add files via upload

---
+++ LOCK.ino
@@ -0,0 +1,43 @@
+#include <ESP8266WiFi.h>                                                // esp8266 library
+#include <FirebaseArduino.h>                                             // firebase library
+
+#define FIREBASE_HOST "lock-b2a19.firebaseio.com"                         // the project name address from firebase id
+#define FIREBASE_AUTH "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"                    // the secret key generated from firebase
+#define WIFI_SSID "a"                                          // input your home or public wifi name 
+#define WIFI_PASSWORD "password"  //password of wifi ssid
+#define LOCK 2
+
+String fireStatus = "";                                                     // led status received from firebase                                                              // for external led
+void setup() {
+  Serial.begin(9600);
+  delay(1000);   
+  pinMode(LOCK, OUTPUT);                 
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);                                      //try to connect with wifi
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("Connected to ");
+  Serial.println(WIFI_SSID);
+  Serial.print("IP Address is : ");
+  Serial.println(WiFi.localIP());                                                      //print local IP address
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);                                       // connect to firebase
+}
+
+void loop() {
+  fireStatus = Firebase.getString("UpLock/Status");                                       // get ld status input from firebase
+  if (fireStatus == "\"UNLOCKED\"") {                                                          // compare the input of led status received from firebase
+    Serial.println("Unlocking");                                                                          // make bultin led ON
+    digitalWrite(LOCK, LOW);                                                        // make external led ON
+  } 
+  else if (fireStatus =="\"LOCKED\"") {                                                  // compare the input of led status received from firebase
+    Serial.println("Locking");                                           
+    digitalWrite(LOCK, HIGH);                                                         // make external led OFF
+  }
+  else {
+    Serial.println("Wrong Credentials!");
+  }
+}
